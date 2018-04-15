716b0633a5b06e9fa6c6c3974d0f298164105dc1
jqiao2 <jqiao2@gmail.com>
2018-04-15 16:06:08 -0700

    Added some functionality, but still need to integrate into the bot

---
+++ MacOSTagRetriever.py
@@ -0,0 +1,17 @@
+from xattr import xattr
+from pathlib import Path
+from biplist import readPlistFromString
+import re
+
+pathlist = Path('/Users/JasonQiao/Documents/Reddit/RedditBots/testingFile/New Folder With Items 2').glob('*.jpg')
+
+for path in pathlist:
+	str_path = str(path)
+	
+	file_name = str_path[81:]
+	print(file_name)
+
+	subreddits = readPlistFromString(xattr(str_path).get('com.apple.metadata:_kMDItemUserTags'))
+	for subreddit in subreddits:
+		print(re.compile('[^a-zA-Z_]+').sub('', subreddit))
+	print()
